28f514beb4f0ee47635a1f61db864b9b6bb29983
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-09 15:27:03 +0300

    END OF CHAPTER 2 : Route Constraints -> aspnetCore official documentation can be visited. Route constraints allow us to fortify the endpoints by defining attribute routing

--- App.API/Controllers/ProductsController.cs
+++ App.API/Controllers/ProductsController.cs
@@ -15,22 +15,22 @@ public class ProductsController(IProductService productService) : CustomBaseCont
         return CreateActionResult(await productService.GetAllList());
     }
 
-    [HttpGet("{pageNumber}/{pageSize}")]
+    [HttpGet("{pageNumber:int}/{pageSize:int}")]
     public async Task<IActionResult> GetPagedAll(int pageNumber, int pageSize) => CreateActionResult(await productService.GetPagedAllListAsync(pageNumber, pageSize));
 
-    [HttpGet("{id}")]
+    [HttpGet("{id:int}")]
     public async Task<IActionResult> GetProductById(int id) => CreateActionResult(await productService.GetProductByIdAsync(id));
 
     [HttpPost]
     public async Task<IActionResult> Create(CreateProductRequest request) => CreateActionResult(await productService.CreateProductAsync(request));
 
-    [HttpPut("{id}")]
+    [HttpPut("{id:int}")]
     public async Task<IActionResult> Update(int id, UpdateProductRequest request) => CreateActionResult(await productService.UpdateProductAsync(id, request));
 
 
     [HttpPatch("stock")]
     public async Task<IActionResult> UpdateStock(UpdateProductStockRequest request) => CreateActionResult(await productService.UpdateStockAsync(request));
 
-    [HttpDelete("{id}")]
+    [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete(int id) => CreateActionResult(await productService.DeleteProductAsync(id));
 }
